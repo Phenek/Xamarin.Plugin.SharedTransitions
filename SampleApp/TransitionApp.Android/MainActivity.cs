8eb491a125d3aaa4b1f78d4882ee479524773bc0
Author Phenek <phenek.github@outlook.com>
2019-03-22 13:39:28 +0100

    - Pass to .Net Standart Library

---
+++ SampleApp/TransitionApp.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Plugin.SharedTransitions.Droid;
+
+namespace TransitionApp.Droid
+{
+    [Activity(Label = "TransitionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            SharedTransition.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ SharedTransitions/SharedTransitions.Android/SharedTransition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+
+namespace Plugin.SharedTransitions.Droid
+{
+    public class SharedTransition
+    {
+        public static void Init(Activity activity, Bundle bundle)
+        {
+        }
+    }
+}
