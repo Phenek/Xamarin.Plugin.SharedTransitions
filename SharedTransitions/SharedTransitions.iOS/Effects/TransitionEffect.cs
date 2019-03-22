6dcd43e1bc703037e09abfa3f95b160b04064281
Author Phenek <phenek.github@outlook.com>
2019-03-22 13:39:28 +0100

    - Pass to .Net Standart Library

---
+++ SharedTransitions/SharedTransitions.iOS/Effects/TransitionEffect.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using Plugin.SharedTransitions;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName(Transition.ResolutionGroupName)]
+[assembly: ExportEffect(typeof(Plugin.SharedTransitions.iOS.Effects.TransitionEffect), Transition.EffectName)]
+
+namespace Plugin.SharedTransitions.iOS.Effects
+{
+    public class TransitionEffect : PlatformEffect
+    {
+
+        protected override void OnAttached()
+        {
+            if (Control == null)
+                return;
+
+            UpdateTag();
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == Transition.TagProperty.PropertyName ||
+                args.PropertyName == Transition.TagGroupProperty.PropertyName)
+                UpdateTag();
+
+            base.OnElementPropertyChanged(args);
+        }
+
+        void UpdateTag()
+        {
+            if (Element is View element)
+                Control.Tag = Transition.RegisterTagInStack(element);
+        }
+    }
+}
---
+++ SharedTransitions/SharedTransitions.iOS/SharedTransition.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Plugin.SharedTransitions.iOS
+{
+    public class SharedTransition
+    {
+        public static void Init()
+        {
+        }
+    }
+}
